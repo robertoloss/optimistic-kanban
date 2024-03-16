d9fcd327604c5e536ad76bdc8fa31e1e0e1c2a53
Roberto Loss <robertoloss@gmail.com>
2024-03-16 15:37:37 -0400

    prisma and supa connected

--- app/page.tsx
+++ app/page.tsx
@@ -1,54 +1,56 @@
-import DeployButton from "../components/DeployButton";
 import AuthButton from "../components/AuthButton";
-import { createClient } from "@/utils/supabase/server";
-import ConnectSupabaseSteps from "@/components/tutorial/ConnectSupabaseSteps";
-import SignUpUserSteps from "@/components/tutorial/SignUpUserSteps";
 import Header from "@/components/Header";
+import { prisma } from "@/prisma/client";
+import { get } from "http";
+
+export const dynamic = 'force-dynamic'
+
+const getProfiles = async () => {
+["getProfiles"]
+try {
+		const profiles = await prisma.profile.findMany();
+		return profiles;
+	} catch( error ) {
+		console.log(error);
+	}
+}
+const getTasks = async () => {
+["getProfiles"]
+try {
+		return await prisma.task.findMany();
+	} catch( error ) {
+		console.log(error);
+	}
+}
 
 export default async function Index() {
-  const canInitSupabaseClient = () => {
-    // This function is just for the interactive tutorial.
-    // Feel free to remove it once you have Supabase connected.
-    try {
-      createClient();
-      return true;
-    } catch (e) {
-      return false;
-    }
-  };
 
-  const isSupabaseConnected = canInitSupabaseClient();
-
+	const profiles = await getProfiles()
+	const tasks = await getTasks()
+  
   return (
     <div className="flex-1 w-full flex flex-col gap-20 items-center">
       <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
-        <div className="w-full max-w-4xl flex justify-between items-center p-3 text-sm">
-          <DeployButton />
-          {isSupabaseConnected && <AuthButton />}
+        <div className="w-full max-w-4xl flex justify-end items-center p-3 text-sm">
+          <AuthButton />
         </div>
       </nav>
 
       <div className="animate-in flex-1 flex flex-col gap-20 opacity-0 max-w-4xl px-3">
         <Header />
         <main className="flex-1 flex flex-col gap-6">
-          <h2 className="font-bold text-4xl mb-4">Next steps</h2>
-          {isSupabaseConnected ? <SignUpUserSteps /> : <ConnectSupabaseSteps />}
+					{profiles && profiles.map(profile => 
+						<h1>{profile.first_name} {profile.last_name}</h1>
+					)}
+					{tasks && tasks.map(task =>
+						<div>
+							<h1>{task.title}</h1>
+							<h1>{task.content}</h1>
+						</div>
+					)}
         </main>
       </div>
 
-      <footer className="w-full border-t border-t-foreground/10 p-8 flex justify-center text-center text-xs">
-        <p>
-          Powered by{" "}
-          <a
-            href="https://supabase.com/?utm_source=create-next-app&utm_medium=template&utm_term=nextjs"
-            target="_blank"
-            className="font-bold hover:underline"
-            rel="noreferrer"
-          >
-            Supabase
-          </a>
-        </p>
-      </footer>
     </div>
   );
 }
