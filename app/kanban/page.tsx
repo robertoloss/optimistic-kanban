d69e5172cbcf72aea9a6c50dd5b9b30ce840ec5e
Roberto Loss <robertoloss@gmail.com>
2024-03-22 11:17:41 -0400

    fix

--- app/kanban/page.tsx
+++ app/kanban/page.tsx
@@ -2,16 +2,12 @@ import AuthButton from "@/components/AuthButton";
 import Kanban from "@/components/kanban/Kanban";
 import { createClient } from "@/utils/supabase/server";
 import { redirect } from "next/navigation";
-import { getColumns, getTasks } from "@/prisma/queries";
 
 export default async function KanbanPage() {
   const supabase = createClient();
   const { data: { user } } = await supabase.auth.getUser();
   if (!user) { return redirect("/login") }
 
-	const tasks = await getTasks()
-	const columns = await getColumns()
-	console.log("KanbanPage")
   return (
     <div className="flex-1 w-full flex flex-col gap-20 items-center">
       <div className="w-full">
@@ -21,7 +17,7 @@ export default async function KanbanPage() {
 						</div>
         </nav>
       </div>
-			<Kanban dbTasks={tasks} dbColumns={columns}/>
+			<Kanban/>
     </div>
   );
 }
