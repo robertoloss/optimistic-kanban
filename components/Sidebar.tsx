894cb6494ad5e62b02ed6aa0fc65dafabbce0704
Roberto Loss <robertoloss@gmail.com>
2024-03-31 12:18:39 -0400

    sidebar responsiveness

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -6,15 +6,15 @@ type Props = {
 export default function Sidebar({ projects }: Props) {
 
 	return (
-		<div className="flex flex-col p-4">
-		<div className={`
-			flex flex-col h-full min-w-[180px] w-[240px] bg-layout rounded-xl p-4
-			border dark:border-none
-		`}>
-			{projects && projects.map(project => (
-				<p>{project.title}</p>
-			))}
-		</div>
+		<div className="hidden md:flex flex-col p-4 ">
+			<div className={`
+				flex flex-col h-full min-w-[80px] max-w-[80px] lg:min-w-[180px] lg:w-[240px] bg-layout rounded-xl p-4
+				border dark:border-none
+			`}>
+				{projects && projects.map(project => (
+					<p>{project.title}</p>
+				))}
+			</div>
 		</div>
 	)
 }
