8f9f2c000feb4fb1c8ca411b2b5719a25bd78607
Roberto Loss <robertoloss@gmail.com>
2024-05-19 00:38:47 -0400

    fix update psw

--- app/update-password/page.tsx
+++ app/update-password/page.tsx
@@ -1,24 +1,23 @@
 'use client'
 import { createClient } from "@/utils/supabase/client";
+import { useRouter } from "next/navigation";
 import { useEffect } from "react";
 
 
 
 export default function UpdatePassword() {
 	const supabase = createClient()
+	const router = useRouter()
 	useEffect(() => {
 		console.log("use effect")
 		supabase.auth.onAuthStateChange(async (event, session) => {
-			if (event == "PASSWORD_RECOVERY") {
-				const newPassword = prompt("What would you like your new password to be?");
-				if (newPassword) {
-					const { data, error } = await supabase.auth
-						.updateUser({ password: newPassword })
-					if (data) alert("Password updated successfully!")
-					if (error) alert("There was an error updating your password.")
-				}
-			} else {
-				console.log("no PASSWORD_RECOVERY", event)
+			const newPassword = prompt("What would you like your new password to be?");
+			if (newPassword) {
+				const { data, error } = await supabase.auth
+					.updateUser({ password: newPassword })
+				if (data) alert("Password updated successfully!")
+				if (error) alert("There was an error updating your password.")
+				router.push(`kanban/home`)
 			}
 		})
 	}, [])
