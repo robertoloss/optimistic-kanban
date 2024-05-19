86e1af482e36d098a7e18680e3cf9446781a6a2b
Roberto Loss <robertoloss@gmail.com>
2024-05-19 00:51:43 -0400

    fixing update logic

--- app/update-password/page.tsx
+++ app/update-password/page.tsx
@@ -13,8 +13,8 @@ export default function UpdatePassword() {
 		supabase.auth.onAuthStateChange(async (event, session) => {
 			const newPassword = prompt("What would you like your new password to be?");
 			if (newPassword) {
-				const { data, error } = await supabase.auth
-					.updateUser({ password: newPassword })
+				console.log("new password")
+				const { data, error } = await supabase.auth.updateUser({ password: newPassword })
 				if (data) alert("Password updated successfully!")
 				if (error) alert("There was an error updating your password.")
 				router.push(`kanban/home`)
